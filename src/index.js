207607a1176284516dd342f8646b22e8b6449adb
mhlee80 <myeonghyeon.lee@gmail.com>
2019-05-10 12:45:31 +0900

    add sample counter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react'; // MobX 에서 사용하는 Provider
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import CounterStore from './stores/counter'; // 방금 만든 스토어 불러와줍니다.
+
+const counter = new CounterStore(); // 스토어 인스턴스를 만들고
+
+ReactDOM.render(
+  <Provider counter={counter}>
+    {/* Provider 에 props 로 넣어줍니다. */}
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
